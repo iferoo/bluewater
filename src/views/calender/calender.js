f01705b4856d94dda18e1e9da2707ec85a02f0d9
Eyad Mohamed <eyadmohamed2859@gmail.com>
2022-09-11 22:41:58 +0000

    finish calender features demo

--- src/views/calender/calender.js
+++ src/views/calender/calender.js
@@ -1,91 +1,88 @@
-import { Fragment, useMemo, useState, useCallback } from 'react';
-
-import PropTypes from 'prop-types';
+import { Fragment, useState, useCallback } from 'react';
 
 import moment from 'moment';
 
-import { Calendar, Views, DateLocalizer, momentLocalizer } from 'react-big-calendar';
+import { Calendar, Views, momentLocalizer } from 'react-big-calendar';
 
 import CalenderDialog from './calenderDialog';
 
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-import events from './event';
-
-const ColoredDateCellWrapper = ({ children }) =>
-    React.cloneElement(React.Children.only(children), {
-        style: {
-            backgroundColor: 'lightblue'
-        }
-    });
-
 const localizer = momentLocalizer(moment);
+
+import events from './event';
 
 export default function Calender() {
     const [myEvents, setEvents] = useState(events);
-
-    //Handle calencder Events
-    const { defaultDate } = useMemo(
-        () => ({
-            components: {
-                timeSlotWrapper: ColoredDateCellWrapper
-            },
-            defaultDate: new Date(),
-            views: Object.keys(Views).map((k) => Views[k])
-        }),
-        []
-    );
-
-    const handleSelectSlot = useCallback(({ start, end }) => {
-        setStatus('ADD');
-        setTitle('');
-        setStart(start);
-        setEnd(end);
-        setOpenDialog(true);
-    }, []);
-
-    const handleSelectEvent = useCallback(({ title, start, end }) => {
-        setStatus('UPDATE');
-        setTitle(title);
-        setStart(start);
-        setEnd(end);
-        setOpenDialog(true);
-    }, []);
 
     //Handle Dialog Variable
     const [status, setStatus] = useState('ADD');
     const [openDialog, setOpenDialog] = useState(false);
     const [title, setTitle] = useState('');
-    const [start, setStart] = useState(null);
-    const [end, setEnd] = useState(null);
+    const [start, setStart] = useState(new Date());
+    const [end, setEnd] = useState(new Date());
+    const [ID, setId] = useState(null);
 
+    const handleSelectSlot = ({ start, end }) => {
+        setTitle('');
+        setStart(start);
+        setEnd(end);
+        setStatus('ADD');
+        setOpenDialog(true);
+    };
+
+    const handleSelectEvent = ({ id, title, start, end }) => {
+        setStatus('UPDATE');
+        setTitle(title);
+        setStart(start);
+        setEnd(end);
+        setId(id);
+        setOpenDialog(true);
+    };
     //Handle Calender Task submit
     const handleSubmite = useCallback(
-        (status, title, start, end) => {
-            if (status === 'ADD') {
-                setEvents([...events, { start, end, title }]);
-            } else {
-                console.log('object');
+        (status, title, start, end, id) => {
+            let newEvents = null;
+            switch (status) {
+                case 'ADD':
+                    setEvents([...myEvents, { id: Math.random(), start, end, title }]);
+                    break;
+                case 'UPDATE':
+                    const eventIndex = myEvents.findIndex((event) => event.id === id);
+                    newEvents = myEvents;
+                    newEvents.splice(eventIndex, 1, { id, start, end, title });
+                    setEvents(newEvents);
+                    break;
+                case 'DELETE':
+                    newEvents = myEvents.filter((event) => event.id !== id);
+                    setEvents(newEvents);
+
+                    break;
+                default:
+                    break;
             }
         },
-        [setEvents]
+        [myEvents]
     );
 
     return (
         <Fragment>
             <CalenderDialog
+                id={ID}
                 taskTitle={title}
                 openDialog={openDialog}
                 setOpenDialog={setOpenDialog}
-                start={start || undefined}
-                end={end || undefined}
+                start={start}
+                setStart={setStart}
+                end={end}
+                setEnd={setEnd}
                 status={status}
                 handleSubmite={handleSubmite}
             />
             <Calendar
                 dayLayoutAlgorithm="no-overlap"
-                defaultDate={defaultDate}
+                defaultDate={new Date()}
                 events={myEvents}
                 localizer={localizer}
                 onSelectEvent={handleSelectEvent}
@@ -99,7 +96,3 @@ export default function Calender() {
         </Fragment>
     );
 }
-Calender.propTypes = {
-    localizer: PropTypes.instanceOf(DateLocalizer),
-    showDemoLink: PropTypes.bool
-};
--- src/views/calender/calenderDialog.js
+++ src/views/calender/calenderDialog.js
@@ -1,21 +1,25 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import { useTheme } from '@mui/material/styles';
-import { TextField } from '@mui/material';
+
+import { Stack, TextField } from '@mui/material';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 
-import PropTypes from 'prop-types';
-
-export default function CalenderDialog({ taskTitle, openDialog, setOpenDialog, start, end, status, handleSubmite }) {
+export default function CalenderDialog({ id, taskTitle, openDialog, setOpenDialog, start, setStart, end, setEnd, status, handleSubmite }) {
     const theme = useTheme();
+
+    const [title, setTitle] = useState(taskTitle);
+
     const handleClose = () => {
         setOpenDialog(false);
     };
-    const [title, setTitle] = useState('');
 
+    // const date = new Date();
+    // console.log(new Date(start.toISOString().slice(0, 16)), start.toISOString());
     return (
         <div>
             <Dialog
@@ -28,24 +32,54 @@ export default function CalenderDialog({ taskTitle, openDialog, setOpenDialog, s
                     {'Task'}
                 </DialogTitle>
                 <DialogContent>
-                    <TextField
-                        id="outlined-basic"
-                        label="Task Name"
-                        variant="outlined"
-                        sx={{ mt: 2 }}
-                        defaultValue={taskTitle}
-                        onChange={(event) => setTitle(event.target.value)}
-                    />
+                    <Stack>
+                        <TextField
+                            id="outlined-basic"
+                            label="Task Name"
+                            variant="outlined"
+                            sx={{ mt: 2 }}
+                            defaultValue={taskTitle}
+                            onChange={(event) => setTitle(event.target.value)}
+                        />
+                        <TextField
+                            id="outlined-basic"
+                            label="Start Date"
+                            variant="outlined"
+                            type="datetime-local"
+                            defaultValue={start.toISOString().slice(0, 16)}
+                            sx={{ mt: 2 }}
+                            onChange={(event) => setStart(new Date(event.target.value))}
+                        />
+                        <TextField
+                            id="outlined-basic"
+                            label="End Date"
+                            variant="outlined"
+                            type="datetime-local"
+                            defaultValue={end.toISOString().slice(0, 16)}
+                            sx={{ mt: 2 }}
+                            onChange={(event) => setEnd(new Date(event.target.value))}
+                        />
+                    </Stack>
                 </DialogContent>
                 <DialogActions>
-                    <Button onClick={handleClose} variant="outlined" color="error">
+                    <Button
+                        variant="outlined"
+                        color="error"
+                        onClick={() => {
+                            const newStatus = status === 'ADD' ? 'CANCEL' : 'DELETE';
+                            handleSubmite(newStatus, title, start, end, id);
+                            handleClose();
+                            setTitle('');
+                        }}
+                    >
                         {status === 'ADD' ? 'Cancel' : 'Delete'}
                     </Button>
                     <Button
                         variant="contained"
                         onClick={() => {
-                            handleSubmite(status, title, start, end);
+                            handleSubmite(status, title, start, end, id);
                             handleClose();
+                            setTitle('');
                         }}
                     >
                         {status === 'ADD' ? 'Add' : 'Update'}
@@ -56,11 +90,14 @@ export default function CalenderDialog({ taskTitle, openDialog, setOpenDialog, s
     );
 }
 CalenderDialog.propTypes = {
+    id: PropTypes.number,
     taskTitle: PropTypes.string,
     openDialog: PropTypes.bool,
     setOpenDialog: PropTypes.func,
     start: PropTypes.instanceOf(Date),
     end: PropTypes.instanceOf(Date),
     status: PropTypes.string,
-    handleSubmite: PropTypes.func
+    handleSubmite: PropTypes.func,
+    setStart: PropTypes.func,
+    setEnd: PropTypes.func
 };
